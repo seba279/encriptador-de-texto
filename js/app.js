07aa0678a0f8260353f62c8d132ef4b1f7f98bf4
Seba Dorado <sebadorado@hotmail.com>
2024-07-11 20:23:58 -0300

    Actualizancion del archivo app.js

--- js/app.js
+++ js/app.js
@@ -4,24 +4,106 @@ let btnEncriptar = document.getElementById('botonEncriptar');
 let btnDesencriptar = document.getElementById('botonDesencriptar');
 let listaFrases = document.getElementById('listaFrases');
 
+//Creando un Arreglo
 let frases = [];
 
+//EVENTOS
 
-//EVENTO
-eventListener();
+//Carga configuraciones iniciales
+document.addEventListener("DOMContentLoaded", configuracionInicial());
+
+//Permite agregar una frase a un arreglo.
+btnEncriptar.addEventListener("click", agregarFrase);
+
+//Permite desencriptar la frase al hacer click sobre el boton.
+btnDesencriptar.addEventListener('click', desencriptarTexto);
 
 //FUNCIONES
 
-function eventListener() {
+function configuracionInicial() {
     texto.focus();
-    btnEncriptar.addEventListener("click", agregarFrase);
-    //btnDesencriptar.addEventListener("click", desencriptarTexto);
+    feather.replace();
+}
+
+function mostrarError(error) {
+    mensaje.style.display = "flex";
+    mensaje.textContent = error;
+    setTimeout(() => { 
+        mensaje.style.display = "none";
+        texto.focus();
+    }, 2000);
+    texto.value = "";
+}
+
+function encriptarVocales(texto){
+    return texto.replaceAll('a', 'ai')
+                .replaceAll('e', 'enter')
+                .replaceAll('i', 'imes')
+                .replaceAll('o', 'ober')
+                .replaceAll('u', 'ufat');
+}
+
+//Funcion para encriptar la frase ingresada
+function encriptarTexto() {
+    let frase = texto.value;
+    return encriptarVocales(frase);
+}
+
+function crearElementos() {
+
+    listaFrases.innerHTML="";
+
+    if(frases.length > 0){
+
+        frases.forEach((frase) => {
+
+            //Creando un div
+            const div = document.createElement('div');
+            div.classList.add('listaFrases');
+            div.innerHTML = frase;
+
+            //Creando el boton
+            const boton = document.createElement('button');
+
+            //Creando una clase para el boton
+            boton.className = 'contenido__encriptador__boton_copiar';
+
+            //Evento click del boton que nos permite copiar el texto
+            boton.addEventListener("click", () => {
+                const fraseEncriptada = frase;
+                //console.log(fraseEncriptada);
+                navigator.clipboard.writeText(fraseEncriptada);
+                texto.value="";
+            });
+
+            //Creando el icono
+            const icono = document.createElement('i');
+
+            //Asignandole un icono al boton(feather)
+            icono.setAttribute("data-feather", "copy");
+
+            //Creando una clase para el icono
+            icono.className = "listaFrases__icono_copiar";
+
+            boton.appendChild(icono);
+
+            div.appendChild(boton);
+
+            listaFrases.appendChild(div);
+            /* Iconos Feather */
+            feather.replace();
+        })
+    }
+}
+
+function mostrarResultado(elemento, frase){
+    let elementoHTML = document.getElementsByClassName(elemento);
+    elementoHTML.innerHTML = frase;
+    return;
 }
 
 function agregarFrase() {
 
-    //let frases = [];
-    console.log(frases);
     let frase = texto.value;
     
     if(frase === ""){
@@ -41,141 +123,57 @@ function agregarFrase() {
         return;
     }else {
         const fraseEncriptada = encriptarTexto(frase);
+        //console.log(fraseEncriptada);
         frases = [...frases,fraseEncriptada];
         //console.log(frases);
         texto.value = "";
         texto.focus();
         crearElementos();
-    }
-    
-}
-
-function mostrarError(error) {
-    const mensajeError = document.createElement('div');
-    mensajeError.textContent = error;
-    mensajeError.classList.add('contenedor');
-
-    const contenedor = document.querySelector('#contenedor1');
-    contenedor.appendChild(mensajeError);
-    setTimeout(() => { 
-        mensajeError.remove();
-        texto.focus();
-    }, 2000);
-    texto.value = "";
-   
-}
-
-function crearElementos() {
-
-    listaFrases.innerHTML="";
-
-    if(frases.length > 0){
-        frases.forEach((frase) => {
-            const div = document.createElement('div');
-            div.classList.add('listaFrases');
-            div.innerHTML = frase;
-             // Crear un checkbox
-            const checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.className = 'styled-checkbox';
-            checkbox.addEventListener('change', () => {
-                if (checkbox.checked) {
-
-                    //Deshabilitar todos los demás checkboxes
-                    const allCheckboxes = document.querySelectorAll('.styled-checkbox');
-                    allCheckboxes.forEach(cb => {
-                        if (cb !== checkbox) {
-                            cb.disabled = true;
-                        }
-                        setTimeout(() => {
-                            cb.checked = false;
-                            cb.disabled = false;
-                        }, 3000);
-                        
-                    });
-
-                    const textoEncriptado = div.innerText;
-                    //console.log(textoEncriptado);
-                    
-                    //setTimeout(() => {
-                      //  checkbox.checked = false; 
-                    //}, 2000);
-                    
-                    desencriptarTexto(textoEncriptado);
-                    
-                    setTimeout(() => {
-                        texto.focus();
-                    }, 3000);
-                    
-                   
-                    //const resultado = desencriptarTexto(textoEncriptado);
-                    //console.log(resultado);
-
-                    //btnDesencriptar.style.display = 'block';
-                    //btnEncriptar.style.backgroundColor = "red";
-                    //btnEncriptar.style.border ="none";
-                    //btnEncriptar.setAttribute("disabled", "true");
-                    //btnEncriptar.style.color = "white";
-                    //texto.disabled = "true";
-                    //texto.focus();
-                    
-                } else {
-                    // Habilitar todos los checkboxes si se desmarca
-                    const allCheckboxes = document.querySelectorAll('.styled-checkbox');
-                    
-                    allCheckboxes.forEach(cb => {
-                            cb.disabled = false;
-                    });
-                    btnDesencriptar.style.display = 'none';
-                    texto.removeAttribute('disabled');
-                    
-                    //btnEncriptar.style.backgroundColor = "black";
-                    //btnEncriptar.removeAttribute("disabled");
-                    //const textoEncriptado = div.innerText;
-                    //console.log(textoEncriptado);
-
-                }
-            });
-            div.appendChild(checkbox);
-
-            listaFrases.appendChild(div);
-        })
+        console.log(frases);
+        mostrarResultado('listaFrases', fraseEncriptada)
     }
 }
 
-//Funcion para encriptar la frase ingresada
-function encriptarTexto() {
+function desencriptarVocales(texto){
+    return texto.replaceAll('enter','e')
+                .replaceAll('imes','i')
+                .replaceAll('ai','a')
+                .replaceAll('ober','o')
+                .replaceAll('ufat','u'); 
+}
 
-    let frases = texto.value.split(' ');
-    let fraseEncriptada = [];
-
-    frases.forEach(frase => {
-        frase = frase.replaceAll('e','enter');
-        frase = frase.replaceAll('i','imes');
-        frase = frase.replaceAll('a','ai');
-        frase = frase.replaceAll('o','ober');
-        frase = frase.replaceAll('u','ufat');      
-        
-        fraseEncriptada.push(frase);   
-    });
-    
-    let resultado = fraseEncriptada.join(' ');
-    return resultado;
+//Verificando si la frase ingresada esta encriptada o no
+function validarEncriptado() {
+    let texto = document.getElementById('textAreaEncriptada').value;
+    //console.log(texto);
+    let patronEncriptado = /ai|emi|iai|omi|umi/;
+    let estaEncriptado = patronEncriptado.test(texto);
+    //console.log(estaEncriptado);
+    if(!estaEncriptado){
+        mostrarError("El texto no esta encriptado");
+    }else {
+        const textoDesencriptado = desencriptarVocales(texto); 
+        mensaje.style.display = 'flex';
+        document.getElementById('mensaje').innerText = textoDesencriptado;
+        setTimeout(() => {
+            mensaje.style.display = 'none';
+        }, 3000);
+    }
 }
 
 //Funcion para desencriptar la frase ingresada en el input
-function desencriptarTexto(textoEncriptado) {  
+function desencriptarTexto() {  
+    const fraseEncriptada = texto.value;
 
-    const textoDesencriptado = textoEncriptado.replaceAll('enter','e')
-                                        .replaceAll('imes','i')
-                                        .replaceAll('ai','a')
-                                        .replaceAll('ober','o')
-                                        .replaceAll('ufat','u');  
-    mensaje.style.display = 'flex';
-    document.getElementById('mensaje').innerText = textoDesencriptado;
-    setTimeout(() => {
-        mensaje.style.display = 'none';
-    }, 3000);
+    if(fraseEncriptada === ""){
+        mostrarError("Ingrese un frase por favor");
+    }else {
+        validarEncriptado();
+        texto.value= "";
+        setTimeout(() => {
+            texto.focus(); 
+        }, 3000);
+    }
     return;    
 }
 
